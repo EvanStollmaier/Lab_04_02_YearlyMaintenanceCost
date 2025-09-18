a3318e515d5d3fe5f3e8e8690d58c173ef7548dd
EvanStollmaier <yakxtrafn@gmail.com>
2025-09-18 14:52:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    double springCost = 10000;
+    double summerCost = 8000;
+    double fallCost = 6000;
+    double winterCost = 12000;
+    double annualCost = springCost + summerCost + fallCost + winterCost;
+    System.out.println("The annual cost is " + springCost + " for spring, " + summerCost + " for summer, " + fallCost + " for fall, and " + winterCost + " for winter, totaling " + annualCost);
+    }
+}
